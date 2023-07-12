f179beb62096208f1911e9c642f523875d649c4e
Tansuluuu <tynalievatan@gmail.com>
2023-07-12 16:24:37 +0600

    made javascript home work

---
+++ script.js
@@ -0,0 +1,122 @@
+
+// 1
+// function yellowFunction(user) {
+//     if(user) {
+//         return`user admin true welcome, ${user}`
+//     } else {
+//         return 'false not correct user'
+//     }
+// }
+// console.log(yellowFunction(user='Tansuluu'))
+
+// 2
+// function yelliwFunction(day) {
+//     if (day<= 28 ){
+//         return `today is ${day} of february`
+//     } else {
+//         return `on february only 28 day, your day is \`${day}\` of another month`
+//     }
+//
+// }
+// console.log(yelliwFunction(day=66))
+
+// 3
+// function yellowFunction(num) {
+//     if(num === 50){
+//         return`${num} is equals of 50`
+//     } else {
+//         return ` ${num} is not correct`
+//     }
+// }
+// console.log(yellowFunction(num=3))
+
+// 4
+// function yellowFunction(user,age) {
+//     if(user && age >=18){
+//         return `${user} ${age} old, welcome`
+//     } else{
+//         return ` ${user} ${age} old, rejected`
+//     }
+// }
+// console.log(yellowFunction(user= 'oleg', age='3'))
+
+// 5
+// function yellowFunction(user) {
+//     if(user.length >=3 && user.length <6 ){
+//         return` ${user} is too short`
+//     } else {
+//         return "accepted"
+//     }
+// }
+// console.log(yellowFunction('hhh'))
+//
+
+// 6
+// function yellowFunction(age) {
+//     if(typeof age == "number"){
+//         return 'its number'
+//     } else {
+//         return 'its string'
+//     }
+// }
+//
+// console.log(yellowFunction( 30))
+
+// function yellowFunction(age) {
+//     if(typeof age == "string"){
+//         return 'its string'
+//     } else {
+//         return 'its number'
+//     }
+// }
+// console.log(yellowFunction( 20))
+
+
+// function yellowFunction(age) {
+//     if(typeof age == "number"){
+//         return 'its number'
+//     } else {
+//         return 'its string'
+//     }
+// }
+// console.log(yellowFunction( 20))
+
+
+// 7
+// function yellowFunction(age) {
+//
+//     age.toString()
+//
+//     if (age >= 100) {
+//         return 'damn elder, how old are you? :0'
+//     }
+//     if(age >=23){
+//         return 'welcome friend'
+//     } else {
+//         return 'you must be older than 23!'
+//     }
+// }
+// console.log(yellowFunction(20))
+
+//8
+// function yellowFunction(day) {
+//     if(day === 1){
+//         return 'today is monday'
+//     }else if(day === 2){
+//         return 'today is tuesday'
+//     }else if(day === 3){
+//         return 'today is wednesday'
+//     }else if(day === 4){
+//         return 'today is thursday'
+//     }else if(day === 5){
+//         return 'today is friday'
+//     }else if(day === 6-7){
+//        return 'weekend'
+//     } else {
+//         return 'max days of weeks 7'
+//     }
+//
+// }
+//
+// console.log(yellowFunction(11))
+//
